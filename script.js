996c9fc4108692a0c467e9829c8b45331d171123
Kauan Lauer <143208136+kauanlauer@users.noreply.github.com>
2023-10-18 22:14:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Inicialize o Firebase com as configurações do seu projeto
-const firebaseConfig = {
-    apiKey: "AIzaSyCmPWE7wjKuL-iH_Plq4Dn5Va5z-xjGSxU",
+const firebaseConfig = { 
+    apiKey : "AIzaSyCmPWE7wjKuL-iH_Plq4Dn5Va5z-xjGSxU" , 
     authDomain: "compras-db75b.firebaseapp.com",
     databaseURL: "https://compras-db75b-default-rtdb.firebaseio.com",
     projectId: "compras-db75b",
     storageBucket: "compras-db75b.appspot.com",
-    messagingSenderId: "349408434820",
-    appId: "1:349408434820:web:c105e1d21dc053f5bec0d1"
+    mensagensSenderId : "349408434820" , 
+    appId : "1:349408434820:web:c105e1d21dc053f5bec0d1" 
   };
 
 firebase.initializeApp(firebaseConfig);
@@ -14,7 +14,7 @@ firebase.initializeApp(firebaseConfig);
 // Função para mostrar o formulário de registro
 function showRegistrationForm() {
     document.getElementById('loginForm').style.display = 'none';
-    document.getElementById('registrationForm').style.display = 'block';
+    document.getElementById('registrationForm').style display = 'block';
 }
 
 // Função para mostrar o formulário de login
@@ -30,36 +30,35 @@ function showMessage(message, success) {
 
     messageDiv.textContent = message;
     feedbackDiv.style.display = 'block';
-    
+
     if (success) {
-        feedbackDiv.style.backgroundColor = '#ffffff'; // Fundo branco para mensagens de sucesso
-        messageDiv.style.color = '#000000'; // Texto preto para mensagens de sucesso
+        feedbackDiv.style.backgroundColor = '#ffffff';
+        messageDiv.style.color = '#000000';
     } else {
-        feedbackDiv.style.backgroundColor = '#f44336'; // Cor de fundo vermelho para mensagens de erro
-        messageDiv.style.color = '#000000'; // Texto preto para mensagens de erro
+        feedbackDiv.style.backgroundColor = '#f44336';
+        messageDiv.style.color = '#000000';
     }
 }
 
-
+// Função para registrar um usuário
 function register() {
     const email = document.getElementById('registrationEmail').value;
     const password = document.getElementById('registrationPassword').value;
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
-            // Registro bem-sucedido
             const user = userCredential.user;
             showMessage('Registro bem-sucedido. Você pode fazer login agora.', true);
             showLoginForm();
         })
         .catch((error) => {
-            // Lidar com erros de registro
             const errorCode = error.code;
             const errorMessage = error.message;
             showMessage('Erro de registro: ' + errorCode + ' - ' + errorMessage, false);
         });
 }
 
+// Função para fazer login
 function login() {
     const email = document.getElementById('loginEmail').value;
     const password = document.getElementById('loginPassword').value;
@@ -70,15 +69,11 @@ function login() {
             return firebase.auth().signInWithEmailAndPassword(email, password);
         })
         .then((userCredential) => {
-            // Login bem-sucedido
             const user = userCredential.user;
             showMessage('Login bem-sucedido. Bem-vindo, ' + user.email + '!', true);
-
-            // Redirecionar o usuário para a página principal (substitua 'pagina-principal.html' pelo URL desejado)
             window.location.href = 'lista.html';
         })
         .catch((error) => {
-            // Lidar com erros de login
             const errorCode = error.code;
             const errorMessage = errorCode === 'auth/wrong-password' || errorCode === 'auth/user-not-found' ? 'Usuário ou senha errados' : error.message;
             showMessage('Erro de login: ' + errorMessage, false);
@@ -91,12 +86,9 @@ function resetPassword() {
 
     firebase.auth().sendPasswordResetEmail(email)
         .then(() => {
-            // E-mail de redefinição de senha enviado com sucesso
             showMessage('E-mail de redefinição de senha enviado. Verifique sua caixa de entrada.', true);
-            // Você pode redirecionar o usuário para uma página de confirmação aqui
         })
         .catch((error) => {
-            // Lidar com erros de redefinição de senha
             const errorCode = error.code;
             const errorMessage = error.message;
             showMessage('Erro ao enviar o e-mail de redefinição de senha: ' + errorCode + ' - ' + errorMessage, false);
@@ -115,18 +107,18 @@ function backToLoginForm() {
     document.getElementById('resetPasswordForm').style.display = 'none';
     document.getElementById('loginForm').style.display = 'block';
 }
+
+// Função para fazer login com o Google
 function loginWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
-    
+
     firebase.auth().signInWithPopup(provider)
         .then((userCredential) => {
-            // Login bem-sucedido com o Google
             const user = userCredential.user;
             showMessage('Login com o Google bem-sucedido. Bem-vindo, ' + user.displayName + '!', true);
-            // Redirecione o usuário ou execute outras ações necessárias.
+            window.location.href = 'lista.html'; // Redirecione o usuário ou execute outras ações necessárias.
         })
         .catch((error) => {
-            // Lidar com erros de login com o Google
             const errorCode = error.code;
             const errorMessage = error.message;
             showMessage('Erro ao fazer login com o Google: ' + errorCode + ' - ' + errorMessage, false);
