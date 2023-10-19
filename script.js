cae330fb0ecac7a64f93d1ca00d3edd4ccc7d5c6
Kauan Lauer <143208136+kauanlauer@users.noreply.github.com>
2023-10-18 22:18:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 // Inicialize o Firebase com as configurações do seu projeto
-const firebaseConfig = { 
-    apiKey : "AIzaSyCmPWE7wjKuL-iH_Plq4Dn5Va5z-xjGSxU" , 
+const firebaseConfig = {
+    apiKey: "AIzaSyCmPWE7wjKuL-iH_Plq4Dn5Va5z-xjGSxU",
     authDomain: "compras-db75b.firebaseapp.com",
     databaseURL: "https://compras-db75b-default-rtdb.firebaseio.com",
     projectId: "compras-db75b",
     storageBucket: "compras-db75b.appspot.com",
-    mensagensSenderId : "349408434820" , 
-    appId : "1:349408434820:web:c105e1d21dc053f5bec0d1" 
+    messagingSenderId: "349408434820",
+    appId: "1:349408434820:web:c105e1d21dc053f5bec0d1"
   };
 
 firebase.initializeApp(firebaseConfig);
 
+// Resto do seu código aqui...
+
 // Função para mostrar o formulário de registro
 function showRegistrationForm() {
     document.getElementById('loginForm').style.display = 'none';
-    document.getElementById('registrationForm').style display = 'block';
+    document.getElementById('registrationForm').style.display = 'block';
 }
 
 // Função para mostrar o formulário de login
@@ -42,21 +44,29 @@ function showMessage(message, success) {
 
 // Função para registrar um usuário
 function register() {
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
     const email = document.getElementById('registrationEmail').value;
     const password = document.getElementById('registrationPassword').value;
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
         .then((userCredential) => {
+            // Registro bem-sucedido
             const user = userCredential.user;
             showMessage('Registro bem-sucedido. Você pode fazer login agora.', true);
             showLoginForm();
+
+            // Aqui você pode armazenar o nome do usuário (primeiro e último) no banco de dados, se desejar.
+            // Certifique-se de configurar um banco de dados Firebase para fazer isso.
         })
         .catch((error) => {
+            // Lidar com erros de registro
             const errorCode = error.code;
             const errorMessage = error.message;
             showMessage('Erro de registro: ' + errorCode + ' - ' + errorMessage, false);
         });
 }
+
 
 // Função para fazer login
 function login() {
